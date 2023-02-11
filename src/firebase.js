34af09aa7219b37c0f24cee52dee671ef0987181
Akshay Jagiasi <akshayjagiasi1804@gmail.com>
2023-02-11 14:11:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,11 @@
-//firebase sdk
+import firebase from 'firebase';
+
+//firebase SDK here
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebaseApp.auth();
+
+export { db, auth };
